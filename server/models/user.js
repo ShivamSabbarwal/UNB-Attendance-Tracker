7525cf44a41c35b06b45a4b561323acdc1513ff9
Tyler Sargent <tsargent@unb.ca>
2017-10-15 20:04:45 -0300

    REBASE: Changes to user.js
    -Removed first and last names
    -Added e-mail field: stores part before '@'

--- server/models/user.js
+++ server/models/user.js
@@ -3,10 +3,9 @@ const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
   username: { type: 'String', required: true, unique: true },
+  email: { type: 'String', required: true, unique: true },
   // This is very very not secure and definitely needs to be updated soon
   password: { type: 'String', required: true, minlength: 6, maxlength: 20 },
-  firstName: { type: 'String' },
-  lastName: { type: 'String' },
   isAdmin: { type: 'Boolean', default: false },
 });
 export default mongoose.model('User', UserSchema);
