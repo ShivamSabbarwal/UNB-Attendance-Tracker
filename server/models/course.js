8bf9b724869362764851275e6fd899e29ce1d25d
Tyler Sargent <tsargent@unb.ca>
2017-10-15 18:19:04 -0300

    Add files via upload
    FEAT: Write schema for courses and link with profs/students

---
+++ server/models/course.js
@@ -0,0 +1,12 @@
+import mongoose from 'mongoose';
+const Schema = mongoose.Schema;
+
+const CourseSchema = new Schema({
+  courseName: { type: 'String', required: true },
+  professor: { type: 'String', required: true },
+  usernames: { type: 'Array', required: true }, // Usernames of students who are in the class will be in here to link
+  timestamp: { type: 'Date', default: Date.now },
+  // SCHEMA GOES HERE
+});
+
+export default mongoose.model('Course', CourseSchema);
