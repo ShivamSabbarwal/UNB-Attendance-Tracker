65ccf72a3511d43bf54d22586d2751aded6c31d8
Tyler Sargent <tsargent@unb.ca>
2017-10-18 22:43:15 -0300

    FIX: Add create/remove course API
    - Made 'usernames' field not required since it will be empty upon creation of a course

--- server/models/course.js
+++ server/models/course.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 const CourseSchema = new Schema({
   title: { type: 'String', required: true, unique: true },
   professor: { type: 'String', required: true },
-  usernames: { type: 'Array'}, // Usernames of students who are in the class will be in here to link
+  usernames: { type: 'Array' }, // Usernames of students who are in the class will be in here to link
   institution: { type: 'String', required: true },
   location: { type: 'String' }, // room the class takes place in
 });
