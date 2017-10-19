c3eb3cbf984bc4fc12d13e2941f8fa553a2cbd4f
rcreagha <rcreagha@unb.ca>
2017-10-18 21:52:09 -0300

    FEAT: modify course schema
    - removed unneeded timestamp field
    - renamed "courseName" field to "title"
    - added fields for location and institution

--- server/models/course.js
+++ server/models/course.js
@@ -2,10 +2,11 @@ import mongoose from 'mongoose';
 const Schema = mongoose.Schema;
 
 const CourseSchema = new Schema({
-  courseName: { type: 'String', required: true },
+  title: { type: 'String', required: true, unique: true },
   professor: { type: 'String', required: true },
   usernames: { type: 'Array', required: true }, // Usernames of students who are in the class will be in here to link
-  timestamp: { type: 'Date', default: Date.now },
+  institution: { type: 'String', required: true },
+  location: { type: 'String' }, // room the class takes place in
 });
 
 export default mongoose.model('Course', CourseSchema);
