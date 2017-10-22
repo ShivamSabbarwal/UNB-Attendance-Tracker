2e2ec9134e526b3075eb67050a0eb9a1be0f3e95
Alex Walsh <awalsh2@unb.ca>
2017-10-22 16:12:45 +0000

    FIX: Replaced mail.com account with gmail account

--- server/controllers/email.controller.js
+++ server/controllers/email.controller.js
@@ -1,33 +1,29 @@
 var nodemailer = require('nodemailer');
 
-
-let transporter = nodemailer.createTransport({
-        host: 'smtp.mail.com',
-        port: 587,
-        secure: false, // true for 465, false for other ports
+export function sendEmail (req, res) {
+    var transporter = nodemailer.createTransport("SMTP",{
+        service: 'gmail',
         auth: {
-            user: 'UNBSWE4103G1',
+            user: 'swe4103g1@gmail.com',
             pass: 'BentonianPhysics'
         }
-});
-export function sendEmail (req, res) {
-if(!req.body.to || !req.body.subject || !req.body.text) {
-    res.status(403).send("Recipient (to), subject, and text are requiered.");
-  }else
+    });
+    
     var mailOptions = {
-      from: 'UNBSWE4103G1@mail.com',
+      from: 'swe4103g1@gmail.com',
       to: req.body.to,
       subject: req.body.subject,
       text: req.body.text
     }
-    transporter.sendMail(mailOptions)
+    
+    transporter.sendMail(mailOptions, function(error, info){
+      if (error) {
+        console.log(error);
+      } else {
+        console.log('Email sent: ' + info.response);
+      }
+});
     res.status(200).end()
 };
 
-transporter.sendMail(mailOptions, function(error, info){
-  if (error) {
-    console.log(error);
-  } else {
-    console.log('Email sent: ' + info.response);
-  }
-});
+
