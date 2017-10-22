a437f557da453e722f1d001c996e04a48e45997a
Alex Walsh <awalsh2@unb.ca>
2017-10-22 14:47:44 +0000

    FIX: Downgraded nodemailer version

--- server/controllers/email.controller.js
+++ server/controllers/email.controller.js
@@ -6,18 +6,22 @@ let transporter = nodemailer.createTransport({
         port: 587,
         secure: false, // true for 465, false for other ports
         auth: {
-            user: UNBSWE4103G1,
+            user: 'UNBSWE4103G1',
             pass: BentonianPhysics
         }
 });
-export function sendEmail (to, subject, text) {
+export function sendEmail (req, res) {
+if(!req.body.to || !req.body.subject || !req.body.text) {
+    res.status(403).send("Recipient (to), subject, and text are requiered.");
+  }else
     var mailOptions = {
       from: 'UNBSWE4103G1@mail.com',
-      to: to,
-      subject: subject,
-      text: text
+      to: req.body.to,
+      subject: req.body.subject,
+      text: req.body.text
     }
     transporter.sendMail(mailOptions)
+    res.status(200).end()
 };
 
 transporter.sendMail(mailOptions, function(error, info){
