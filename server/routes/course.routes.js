41de4d7c48366a67c12bcb32e1fb69dc54d34b1c
rcreagha <rcreagha@unb.ca>
2017-10-23 16:58:58 -0300

    FIX - move new api calls into different files
    - api methods for creating and removing courses were previously in
      createCourse.controller and have been moved to course.controller.
      A few other files were effected by this change.
      This was done to match the changes made by feature_backend_addStudentToCourse

---
+++ server/routes/course.routes.js
@@ -0,0 +1,20 @@
+import { Router } from 'express';
+import * as CourseController from '../controllers/course.controller';
+const router = new Router();
+
+// Add students to a specific course
+router.route('/course/:courseTitle/students').put(CourseController.addStudents);
+
+// Drop students from a specific course
+router.route('/course/:courseTitle/students').delete(CourseController.dropStudents);
+
+// Get list of courses
+router.route('/courseList').get(CourseController.courseList);
+
+// Add a new course
+router.route('/course').post(CourseController.createCourse);
+
+// Drop a course from the database
+router.route('/course').delete(CourseController.removeCourse);
+
+export default router;
