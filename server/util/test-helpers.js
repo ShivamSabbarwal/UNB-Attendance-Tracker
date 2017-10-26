e61cb573030131460d5f6d1b72ed6eb3c3065e6f
Gehad Elrobey <gehadelrobey@gmail.com>
2017-10-26 02:05:52 -0300

    Use the new testing database instead of the local one
    Signed-off-by: Gehad Elrobey <gehadelrobey@gmail.com>

--- server/util/test-helpers.js
+++ server/util/test-helpers.js
@@ -3,7 +3,7 @@ import mockgoose from 'mockgoose';
 
 export function connectDB(t, done) {
   mockgoose(mongoose).then(() => {
-    mongoose.createConnection('mongodb://localhost:27017/mern-test', err => {
+    mongoose.createConnection('mongodb://mern_user:HMPNGty4e2gLYRYM@ds229415.mlab.com:29415/swe4103_testdatabase', err => {
       if (err) t.fail('Unable to connect to test database');
       done();
     });
