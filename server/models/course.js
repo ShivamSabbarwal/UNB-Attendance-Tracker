ee5e7fde4128a69162e31ba22d43a75183779a4e
Alex Walsh <awalsh2@unb.ca>
2017-10-28 20:24:05 +0000

    FEAT: Added place to store daily attendance to course schema - The student attendance will be added to the student list in the course schema

--- server/models/course.js
+++ server/models/course.js
@@ -4,9 +4,10 @@ const Schema = mongoose.Schema;
 const CourseSchema = new Schema({
   title: { type: 'String', required: true, unique: true },
   professor: { type: 'String', required: true },
-  usernames: { type: 'Array' }, // Usernames of students who are in the class will be in here to link
+  usernames: { type: 'Array' }, // Usernames&Attendance of students who are in the class will be in here to link
   institution: { type: 'String', required: true },
   location: { type: 'String' }, // room the class takes place in
+  dailyAttendance: { type: 'Array' }, //Per-day list of absent students
 });
 
 export default mongoose.model('Course', CourseSchema);
