07d6b3c606ba6e4d659dddcac9b235fa7e6a2350
Gehad Elrobey <gehadelrobey@gmail.com>
2017-11-05 23:13:01 -0400

    FIX: Modify get attendance API to work with the new DS
    Signed-off-by: Gehad Elrobey <gehadelrobey@gmail.com>

--- server/controllers/attendance.controller.js
+++ server/controllers/attendance.controller.js
@@ -3,46 +3,57 @@ import courseGrid from '../models/coursegrid';
 import User from '../models/user';
 import SessionUtils from '../util/sessionUtils';
 
+/**
+ * @author Gehad
+ * @param {String} courseTitle course title to update.
+ * @param {String} submissionTime time for recording attendance (date encoded in RFC2822)
+ * @param {List} absentstudents a list of absent students
+ * @param {function(Boolean, String)} callback a callback with two parameters (fail:Boolean, error message:String)
+ * @returns null
+ */
 function saveStudents(courseTitle, submissionTime, absentstudents, callback) {
   let submissionDate = Date.parse(submissionTime)
   if (!submissionDate) {
     callback(true, "Can't parse the submissionTime (Please use a string representing RFC2822)")
   }
   let studentsNotRegistered = []
-  Course.findOne({ 'title' : courseTitle }, 'title, usernames', function(err, course){
-      if(err){
-        callback(true, err)
-      } else if (course) {
-        //THIS IS WRONG!!! I HATE TO WRITE IT THIS WAY!
-        absentstudents.forEach(absentStudent => {
-          let matched = false
-          course.usernames.forEach(attendanceItem => {
-            if (attendanceItem.username === absentStudent) {
-              attendanceItem.absence.push(submissionTime)
-              matched = true
-            }
-          })
-          if (matched === false) {
-            studentsNotRegistered.push(absentStudent)
+  Course.findOne({
+    'title': courseTitle
+  }, 'title, usernames', function(err, course) {
+    if (err) {
+      callback(true, err)
+    } else if (course) {
+      //THIS IS WRONG!!! I HATE TO WRITE IT THIS WAY!
+      absentstudents.forEach(absentStudent => {
+        let matched = false
+        course.usernames.forEach(attendanceItem => {
+          if (attendanceItem.username === absentStudent) {
+            attendanceItem.absence.push(submissionTime)
+            matched = true
           }
         })
-        course.save(function (err, data) {
-          if(err) {
-            callback(true, err)
-          } else {
-            console.log()
-            callback(false, {
-              notRegisteredStudents: studentsNotRegistered
-            })
-          }
-        });
-      } else {
-        callback(true, "Couldn't find the course in the database")
-      }
+        if (matched === false) {
+          studentsNotRegistered.push(absentStudent)
+        }
+      })
+      course.save(function(err, data) {
+        if (err) {
+          callback(true, err)
+        } else {
+          console.log()
+          callback(false, {
+            notRegisteredStudents: studentsNotRegistered
+          })
+        }
+      });
+    } else {
+      callback(true, "Couldn't find the course in the database")
+    }
   })
 }
 
 /**
+ * @author Gehad
  * @param {XMLHTTPRequest} req A request containing the username of a student.
  * @param {XMLHTTPRequest} res Server reponse. If succesful, returns array of courses.
  * @returns null
@@ -70,7 +81,7 @@ export function submitAttendance(req, res) {
               } else {
                 //ACTUAL CODE EXISTS HERE
                 let absentstudents = req.body.absentstudents
-                saveStudents(req.params.courseTitle, req.body.submissionTime, absentstudents, (err, errorMessage)=>{
+                saveStudents(req.params.courseTitle, req.body.submissionTime, absentstudents, (err, errorMessage) => {
                   if (err) {
                     res.status(500).send(errorMessage).end();
                   } else {
@@ -91,58 +102,51 @@ export function submitAttendance(req, res) {
  * @param {XMLHTTPRequest} res Server reponse. If succesful, returns array of courses.
  * @returns null
  */
-export function getAttendance (req, res){
-    SessionUtils.isValidSession(req.cookies.sessionID).then((isValid) => {
-        isValid = true;
-        if (isValid !== true) {
-            res.status(401).end();
-        } else {
-        SessionUtils.isAdmin(req.cookies.sessionID).then((isAdmin) => {
-        isAdmin = true;
-        if (isAdmin !== true) {
+export function getAttendance(req, res) {
+  const requestDate = Date.parse(req.query.date)
+  if (!req.params.courseTitle || !requestDate) {
+    res.status(403).send("Invalid course title or query date (missing or incorrect)").end();
+  } else {
+    SessionUtils.isValidSession(req.cookies.sessionID).then(isValid => {
+      if (isValid !== true) {
+        res.status(401).end();
+      } else {
+        SessionUtils.isAdmin(req.cookies.sessionID).then(isAdmin => {
+          if (isAdmin !== true) {
             res.status(403).send("This API endpoint requires Admin capability").end();
-
-        } else {
-            Course.findOne({ 'title' : req.params.courseTitle }, 'usernames', function(err, course){
-                if(err){
-                    console.error(err)
-                    res.status(400).end();
-                } else if (course) {
-                    var attendanceRecord = [];
-                    console.log(course.usernames.length);
-                    for (var i = 0, len = course.usernames.length; i < len; i++){
-                        console.log('flag');
-                        attendanceRecord[i] = [];
-                        attendanceRecord[i][0] = course.usernames[i][0];
-                        attendanceRecord[i][1] = [];
-                        for (var j = 0, lenj = course.usernames[i].length; j < lenj; j++){
-                            console.log('inner flag');
-                            if(Date.parse(course.usernames[i][1][j]) < (Date.parse(req.params.date)) &&
-                                (Date.parse(course.usernames[i][1][j])) > (Date.parse(req.params.date) - 518400)){
-
-                                attendanceRecord[i][1].push(course.usernames[i][1][j])
-                            }
-                        }
-                    }
-                    var absenceCount = course.usernames;
-                    for (var i = 0, len = absenceCount.length; i < len; i++){
-                        if(absenceCount[i].length = 2){
-                            absenceCount[i][1] = absenceCount[i][1].length
-                        }
-                    }
-
-                    res.status(200).send(
-                        getAttendance: absenceCount,
-                        getAttendance: attendanceRecord
-                    )
-                } else {
-                    res.status(400).send("Course matching \"" + req.params.courseTitle + "\" not found.");
-                }
+          } else {
+            Course.findOne({
+              'title': req.params.courseTitle
+            }, 'usernames', function(err, course) {
+              if (err) {
+                res.status(400).end();
+              } else if (course) {
+                var attendanceRecord = course.usernames.map(userData => {
+                  return {
+                    name: userData.username,
+                    numberofAbsence: userData.absence.filter(date => {
+                      const recordDate = Date.parse(date)
+                      if (!recordDate) {
+                        return false
+                      }
+                      // 518400000 ms is 6 days
+                      return (recordDate =< requestDate && recordDate > requestDate - 518400000)
+                    }).length
+                  }
+                })
+                console.log(attendanceRecord)
+                res.status(200).send({
+                  students: attendanceRecord
+                })
+              } else {
+                res.status(400).send("Course matching \"" + req.params.courseTitle + "\" not found.");
+              }
             });
-        }
+          }
         });
-        }
+      }
     });
+  }
 }
 /**
  * @param {XMLHTTPRequest} req A request containing the username of a student.
@@ -150,46 +154,48 @@ export function getAttendance (req, res){
  * @returns null
  */
 export function reserveSeat(req, res) {
-SessionUtils.isValidSession(req.cookies.sessionID).then((isValid) => {
-        isValid = true;
-        if (isValid !== true) {
-            res.status(401).end();
-           fulfill(false);
-        } else {
-            courseGrid.findOne({ 'title' : req.params.courseTitle }, 'class', function(err, course){
-                if(err){
-                    console.error(err)
-                    res.status(400).end();
-                } else if (course) {
-                    if (courseGrid.class[req.body.seat[0]][req.body.seat[1]] == -1){
-                        res.status(418);
-                    } else {
-                        console.log('flag');
-                        courseGrid.class[req.body.seat[0]][req.body.seat[1]] = req.body.Username;
-                        for (var i = 0, len = courseGrid.class.length; i < len; i++){
-                            for (var j = 0, lenj = course.usernames.length; j < lenj; j++){
-                                if(courseGrid.class[i][j] === req.body.Username){
-                                    courseGrid.class[i][j] = -1;
-                                }
-                            }
-                        }
-                    }
+  SessionUtils.isValidSession(req.cookies.sessionID).then((isValid) => {
+    isValid = true;
+    if (isValid !== true) {
+      res.status(401).end();
+      fulfill(false);
+    } else {
+      courseGrid.findOne({
+        'title': req.params.courseTitle
+      }, 'class', function(err, course) {
+        if (err) {
+          console.error(err)
+          res.status(400).end();
+        } else if (course) {
+          if (courseGrid.class[req.body.seat[0]][req.body.seat[1]] == -1) {
+            res.status(418);
+          } else {
+            console.log('flag');
+            courseGrid.class[req.body.seat[0]][req.body.seat[1]] = req.body.Username;
+            for (var i = 0, len = courseGrid.class.length; i < len; i++) {
+              for (var j = 0, lenj = course.usernames.length; j < lenj; j++) {
+                if (courseGrid.class[i][j] === req.body.Username) {
+                  courseGrid.class[i][j] = -1;
                 }
+              }
+            }
+          }
+        }
 
-                courseGrid.update(
-                      function(err, data){
-                        if (err){
-                          console.error(err)
-                          res.status(403).end()
-                        } else if (data){
-                          res.status(200).end()
+        courseGrid.update(
+          function(err, data) {
+            if (err) {
+              console.error(err)
+              res.status(403).end()
+            } else if (data) {
+              res.status(200).end()
 
-                        } else {
-                          res.status(400).end()
-                        }
-                      }
-                 );
-            });
-		}
-		});
+            } else {
+              res.status(400).end()
+            }
+          }
+        );
+      });
+    }
+  });
 }
