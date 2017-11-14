ad3a53635d68669d67d8829380654a3e89c5cfc1
rcreagha <rcreagha@unb.ca>
2017-11-14 10:35:40 -0400

    Finish documenting login controller
    - modified login documentation
    - added docs for logout
    - added docs for signup
    - added apidoc as a dependency

--- apidoc/api_data.js
+++ apidoc/api_data.js
@@ -1,11 +1,10 @@
 define({ "api": [
   {
-    "type": "post",
-    "url": "login",
-    "title": "Login with username & password, or session",
-    "name": "login",
-    "group": "Login",
-    "description": "<ul> <li>username and password are not checked (and not needed) if a valid session is provided.</li> <li>username and isAdmin are NOT returned if the session is valid.</li> </ul>",
+    "type": "get",
+    "url": "logout",
+    "title": "Logout",
+    "group": "User",
+    "description": "<h2>Logout from current session</h2> <ul> <li>logging out will remove the session record from the database</li> <li>takes no parameters</li> </ul>",
     "header": {
       "fields": {
         "Header": [
@@ -22,7 +21,9 @@ define({ "api": [
             "description": "<p>session cookie</p>"
           }
         ]
-      },
+      }
+    },
+    "parameter": {
       "examples": [
         {
           "title": "Header Example",
@@ -30,6 +31,59 @@ define({ "api": [
           "type": "json"
         }
       ]
+    },
+    "success": {
+      "fields": {
+        "Success 200": [
+          {
+            "group": "Success 200",
+            "optional": false,
+            "field": "200",
+            "description": "<p>Session removed successfully</p>"
+          }
+        ]
+      }
+    },
+    "error": {
+      "fields": {
+        "Error 4xx": [
+          {
+            "group": "Error 4xx",
+            "optional": false,
+            "field": "400",
+            "description": "<p>The provided session was not successfully removed from the database</p>"
+          }
+        ]
+      }
+    },
+    "version": "0.0.0",
+    "filename": "server/controllers/login.controller.js",
+    "groupTitle": "User",
+    "name": "GetLogout"
+  },
+  {
+    "type": "post",
+    "url": "login",
+    "title": "Login",
+    "group": "User",
+    "description": "<h2>Login with username &amp; password, or session</h2> <ul> <li>username and password are not checked (and not needed) if a valid session is provided.</li> <li>username and isAdmin are NOT returned if the session is valid.</li> </ul>",
+    "header": {
+      "fields": {
+        "Header": [
+          {
+            "group": "Header",
+            "optional": false,
+            "field": "Content-Type",
+            "description": "<p>application/json</p>"
+          },
+          {
+            "group": "Header",
+            "optional": false,
+            "field": "Cookie",
+            "description": "<p>session cookie</p>"
+          }
+        ]
+      }
     },
     "parameter": {
       "fields": {
@@ -54,6 +108,11 @@ define({ "api": [
         {
           "title": "Parameter Example",
           "content": "{\n  \"username\": \"admin\",\n  \"password\": \"password\"\n}",
+          "type": "json"
+        },
+        {
+          "title": "Header Example",
+          "content": "{\n  Content-Type: application/json\n  Cookie: sessionID=344d94eb4a904b37fcc82305ab67d14f\n}",
           "type": "json"
         }
       ]
@@ -110,6 +169,113 @@ define({ "api": [
     },
     "version": "0.0.0",
     "filename": "server/controllers/login.controller.js",
-    "groupTitle": "Login"
+    "groupTitle": "User",
+    "name": "PostLogin"
+  },
+  {
+    "type": "post",
+    "url": "signup",
+    "title": "Signup",
+    "group": "User",
+    "description": "<h2>Create account for new user</h2>",
+    "header": {
+      "fields": {
+        "Header": [
+          {
+            "group": "Header",
+            "optional": false,
+            "field": "Content-Type",
+            "description": "<p>application/json</p>"
+          },
+          {
+            "group": "Header",
+            "optional": false,
+            "field": "Cookie",
+            "description": "<p>session cookie</p>"
+          }
+        ]
+      }
+    },
+    "parameter": {
+      "fields": {
+        "Parameter": [
+          {
+            "group": "Parameter",
+            "type": "String",
+            "optional": false,
+            "field": "username",
+            "description": "<p>Username provided by the user (must be unique)</p>"
+          },
+          {
+            "group": "Parameter",
+            "type": "String",
+            "optional": false,
+            "field": "password",
+            "description": "<p>Password provided by the user</p>"
+          },
+          {
+            "group": "Parameter",
+            "type": "String",
+            "optional": false,
+            "field": "email",
+            "description": "<p>Email provided by the user, without @unb.ca (must be unique)</p>"
+          },
+          {
+            "group": "Parameter",
+            "type": "Boolean",
+            "optional": false,
+            "field": "isAdmin",
+            "defaultValue": "False",
+            "description": "<p>Whether user is an admin</p>"
+          }
+        ]
+      },
+      "examples": [
+        {
+          "title": "Parameter Example",
+          "content": "{\n  \"usename\": \"administrator\",\n  \"password\": \"entropy\",\n  \"email\": \"admin.01\",\n  \"isAdmin\": True\n}",
+          "type": "json"
+        },
+        {
+          "title": "Header Example",
+          "content": "{\n  Content-Type: application/json\n  Cookie: sessionID=344d94eb4a904b37fcc82305ab67d14f\n}",
+          "type": "json"
+        }
+      ]
+    },
+    "success": {
+      "fields": {
+        "Success 200": [
+          {
+            "group": "Success 200",
+            "optional": false,
+            "field": "200",
+            "description": "<p>User created successfully</p>"
+          }
+        ]
+      }
+    },
+    "error": {
+      "fields": {
+        "Error 4xx": [
+          {
+            "group": "Error 4xx",
+            "optional": false,
+            "field": "403",
+            "description": "<p>required arguments are missing, or password is length is not &gt;=6 &amp; &lt;=20 characters, or user could not be created (username or email may be taken)</p>"
+          },
+          {
+            "group": "Error 4xx",
+            "optional": false,
+            "field": "400",
+            "description": "<p>user not created, but no error thrown by the database</p>"
+          }
+        ]
+      }
+    },
+    "version": "0.0.0",
+    "filename": "server/controllers/login.controller.js",
+    "groupTitle": "User",
+    "name": "PostSignup"
   }
 ] });
--- server/controllers/login.controller.js
+++ server/controllers/login.controller.js
@@ -5,9 +5,43 @@ var bigrandom = require('bigrandom');
 
 /**
 *
-* @param req
-* @param res
-* @returns void
+* param req
+* param res
+* returns void
+*
+* @api {post} signup Signup
+* @apiGroup User
+*
+* @apiDescription
+*  ## Create account for new user
+*
+* @apiHeader Content-Type application/json
+* @apiHeader Cookie session cookie
+*
+* @apiParam {String} username         Username provided by the user (must be unique)
+* @apiParam {String} password         Password provided by the user
+* @apiParam {String} email            Email provided by the user, without @unb.ca (must be unique)
+* @apiParam {Boolean} isAdmin=False   Whether user is an admin
+*
+* @apiParamExample {json} Parameter Example
+*     {
+*       "usename": "administrator",
+*       "password": "entropy",
+*       "email": "admin.01",
+*       "isAdmin": True
+*     }
+*
+* @apiParamExample {json} Header Example
+*  {
+*    Content-Type: application/json
+*    Cookie: sessionID=344d94eb4a904b37fcc82305ab67d14f
+*  }
+*
+* @apiSuccess 200 User created successfully
+*
+* @apiError 403 required arguments are missing, or password is length is not >=6 & <=20 characters, or user could not be created (username or email may be taken)
+* @apiError 400 user not created, but no error thrown by the database
+*
 */
 export function generateUserAccount(req, res) {
   if(!req.body.username || !req.body.password || !req.body.email) {
@@ -121,31 +155,31 @@ function checkCredentials(username, password, callback) {
  * param res
  * returns void
  * 
- * @api {post} login Login with username & password, or session
- * @apiName login
- * @apiGroup Login
+ * @api {post} login Login
+ * @apiGroup User
  *
- * @apiDescription 
+ * @apiDescription
+ *  ## Login with username & password, or session
  *  - username and password are not checked (and not needed) if a valid session is provided.
  *  - username and isAdmin are NOT returned if the session is valid.
  *
  * @apiHeader Content-Type application/json
  * @apiHeader Cookie session cookie
  *
- * @apiHeaderExample {json} Header Example
- *  {
- *    Content-Type: application/json
- *    Cookie: sessionID=344d94eb4a904b37fcc82305ab67d14f
- *  }
- *
- * @apiParam {String} username The username provided by the user
- * @apiParam {String} password The password provided by the user
+ * @apiParam {String} username  The username provided by the user
+ * @apiParam {String} password  The password provided by the user
  *
  * @apiParamExample {json} Parameter Example
  *    {
  *      "username": "admin",
  *      "password": "password"
  *    }
+ *
+ * @apiParamExample {json} Header Example
+ *  {
+ *    Content-Type: application/json
+ *    Cookie: sessionID=344d94eb4a904b37fcc82305ab67d14f
+ *  }
  *
  * @apiSuccess {String} username Users username (only returned if valid session was not provided)
  * @apiSuccess {Boolean} isAdmin Users isAdmin property (only returned if valid session was not provided)
@@ -191,6 +225,29 @@ export function login(req, res) {
     })
 }
 
+/**
+ * @api {get} logout Logout
+ * @apiGroup User
+ *
+ * @apiDescription
+ *  ## Logout from current session
+ *  - logging out will remove the session record from the database
+ *  - takes no parameters
+ *
+ * @apiHeader Content-Type application/json
+ * @apiHeader Cookie session cookie
+ *
+ * @apiParamExample {json} Header Example
+ *  {
+ *    Content-Type: application/json
+ *    Cookie: sessionID=344d94eb4a904b37fcc82305ab67d14f
+ *  }
+ *
+ * @apiSuccess 200 Session removed successfully
+ *
+ * @apiError 400 The provided session was not successfully removed from the database
+ *
+ */
 export function logout(req, res) {
   if (req.cookies.sessionID !== null) {
     Session.remove({ 'sessionId': req.cookies.sessionID }, function(err) {
